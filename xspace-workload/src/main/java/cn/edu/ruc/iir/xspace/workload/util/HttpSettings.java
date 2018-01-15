223d3fc1dc164a777e78d364c13c5751966b2567
taoyouxian <taoyouxian@aliyun.com>
2018-01-15 23:39:12 +0800

    add xspace-workload module for rainbow

---
+++ xspace-workload/src/main/java/cn/edu/ruc/iir/xspace/workload/util/HttpSettings.java
@@ -0,0 +1,15 @@
+package cn.edu.ruc.iir.xspace.workload.util;
+
+/**
+ * @version V1.0
+ * @Package: cn.edu.ruc.iir.xspace.workload.util
+ * @ClassName: HttpSettings
+ * @Description: to store the info needed by http
+ * @author: Tao
+ * @date: Create in 2017-09-30 8:58
+ **/
+public class HttpSettings {
+
+    public static final String PRESTO_QUERY = "http://10.77.40.236:8080/v1/query";
+
+}
---
+++ xspace-workload/src/test/java/cn/edu/ruc/iir/xspace/workload/util/HttpUtilTest.java
@@ -0,0 +1,39 @@
+package cn.edu.ruc.iir.xspace.workload.util;
+
+import cn.edu.ruc.iir.xspace.workload.sql.Query;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @version V1.0
+ * @Package: cn.edu.ruc.iir.xspace.workload.util
+ * @ClassName: HttpUtilTest
+ * @Description: Test http
+ * @author: taoyouxian
+ * @date: Create in 2018-01-15 23:08
+ **/
+public class HttpUtilTest {
+
+
+    @Test
+    public void HttpGetTest() {
+        Object o = HttpUtil.HttpGet(HttpSettings.PRESTO_QUERY);
+        JSONArray jsonArray = JSON.parseArray(o.toString());
+        String queryId = null;
+        String query = null;
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+            if (jsonObject.size() == 8) {
+                queryId = jsonObject.get("queryId").toString();
+                query = jsonObject.get("query").toString();
+                System.out.println(queryId + "\t" + i + "\t" + query);
+//                Query q = new Query(queryId, String.valueOf(i), query);
+                // Parser
+
+            }
+        }
+//        System.out.println(o.toString());
+    }
+}
