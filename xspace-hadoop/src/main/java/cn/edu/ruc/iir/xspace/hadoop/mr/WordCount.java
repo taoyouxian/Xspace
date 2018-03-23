f3174bcca2e0a8d2e9de7fa2374d199cad084d07
taoyouxian <taoyouxian@aliyun.com>
2018-03-23 14:37:47 +0800

    add hadoop-wc

---
+++ xspace-hadoop/src/main/java/cn/edu/ruc/iir/xspace/hadoop/mr/WordCount.java
@@ -0,0 +1,68 @@
+package cn.edu.ruc.iir.xspace.hadoop.mr;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+/**
+ * @version V1.0
+ * @Package: cn.edu.ruc.iir.xspace.hadoop.mr
+ * @ClassName: WordCount
+ * @Description:
+ * @author: tao
+ * @date: Create in 2018-03-23 13:10
+ **/
+public class WordCount {
+
+    public static class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        private IntWritable result = new IntWritable();
+
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path("/2018/wc/data"));
+        FileOutputFormat.setOutputPath(job, new Path("/2018/wc/res"));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
