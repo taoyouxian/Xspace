bc120eb4be96841b2133744319c931539ce480d5
u1095279159872 <1095279159872@tianchi.com>
2018-05-30 19:22:32 +0800

    update pom

---
+++ xspace-retrieval/src/main/java/cn/edu/ruc/iir/xspace/retrieval/Main.java
@@ -0,0 +1,16 @@
+package cn.edu.ruc.iir.xspace.retrieval;
+
+/**
+ * @version V1.0
+ * @Package: cn.edu.ruc.iir.xspace.retrieval
+ * @ClassName: Main
+ * @Description:
+ * @author: taoyouxian
+ * @date: Create in 2018-05-23 19:24
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Retrieval!");
+    }
+}
